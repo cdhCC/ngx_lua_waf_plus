b7dd44827409aa6cb799ea06a6ef2a9d34745071
cdhCC <910997602@qq.com>
2022-04-01 10:42:56 +0800

    create

---
+++ config.lua
@@ -0,0 +1,13 @@
+RulePath = "./wafconf/"
+attacklog = "on"
+logdir = "./logs/"
+UrlDeny="on"
+Redirect="on"
+CookieMatch="on"
+postMatch="on"
+whiteModule="on"
+black_fileExt={"php","jsp"}
+ipWhitelist={"127.0.0.1"}
+ipBlocklist={"1.0.0.1"}
+CCDeny="on"
+CCrate="300/60"
